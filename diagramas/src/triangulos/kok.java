b7234e7ec7cec2d5053e690a213d1da746e7250d
AlvaroCtm <86389654+AlvaroCtm@users.noreply.github.com>
2021-07-15 22:50:05 -0500

    kok

---
+++ diagramas/src/triangulos/kok.java
@@ -0,0 +1,14 @@
+package triangulos;
+
+import java.util.Scanner;
+
+public class kok {
+
+	public static void main(String[] args) {
+		Scanner es = new Scanner(System.in);
+		System.out.println("ola prueba git");
+		
+
+	}
+
+}

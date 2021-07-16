fff1a38ec1da3d5caf9d5e0b56e72c22e5807359
AlvaroCtm <86389654+AlvaroCtm@users.noreply.github.com>
2021-07-16 17:31:59 -0500

    pruebahoy

---
+++ diagramas/src/triangulos/prueba.java
@@ -0,0 +1,12 @@
+package triangulos;
+
+import java.util.Scanner;
+
+public class prueba {
+
+	public static void main(String[] args) {
+	Scanner sc =new Scanner(System.in);
+	System.out.println("hola git");
+	}
+
+}
